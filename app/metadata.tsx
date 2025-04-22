a63b60e2580566dffb6cfbf8b6b518747e5a7cc6
Shashwat <shashwattripathi1503@gmail.com>
2025-04-22 22:30:54 +0530

    First

---
+++ app/metadata.tsx
@@ -0,0 +1,5 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "Aaranya",
+    description: "AI-powered waste management Platform ",
+  };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
+        WEB3_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID,
+        GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+        GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+
+    },
+};
 
 export default nextConfig;
+
